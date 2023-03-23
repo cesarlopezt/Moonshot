d2b209a62abf2edca50504667097e806282c44d5
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-23 18:05:44 -0500

    Load Missions and astronauts and show them

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Cesar Lopez on 3/23/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
