fffbcc75c30b95e55fc34117f9ade3b68ef36998
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-23 22:31:23 -0500

    Switch between List and Grid

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,52 +7,112 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
+struct GridDisplay: View {
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
     
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
                             VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct ListDisplay: View {
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 70, height: 70)
+                            .padding(.horizontal)
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundColor(.white.opacity(0.5))
                         }
                     }
                 }
-                .padding([.horizontal, .bottom])
+            }
+            .listRowBackground(Color.darkBackground)
+        }
+        .listStyle(.plain)
+        .padding([.horizontal, .bottom])
+    }
+}
+
+struct ContentView: View {
+    @State private var isGrid = true
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+
+    var body: some View {
+        NavigationView {
+            Group {
+                if isGrid {
+                    GridDisplay(missions: missions, astronauts: astronauts)
+                } else {
+                    ListDisplay(missions: missions, astronauts: astronauts)
+                }
             }
             .navigationTitle("Moonshot")
+            .toolbar {
+                Button {
+                    isGrid.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: isGrid ? "list.dash" : "rectangle.grid.2x2")
+                    }
+                }
+            }
             .background(.darkBackground)
             .preferredColorScheme(.dark)
         }
